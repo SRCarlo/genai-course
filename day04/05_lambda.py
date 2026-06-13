058e945e600abb606f6e1eac038d5b352572e6c2
SRCarlo <asphaltshubhuu@gmail.com>
2026-06-13 11:48:44 +0530

    feat(day04): add lambda function examples

---
+++ day04/05_lambda.py
@@ -0,0 +1,15 @@
+# LAMBDA FUNCTIONS
+
+#  Normal
+def mul(num):
+    return num * num
+
+# Lamda
+square = lambda num: num * num
+
+# Use
+print(square(5))
+
+# Exa 
+cube = lambda x: x * x * x
+print(cube(10))
