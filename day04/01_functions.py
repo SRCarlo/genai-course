79566694f64a9448021d950f0f723244f48bd151
SRCarlo <asphaltshubhuu@gmail.com>
2026-06-13 11:27:14 +0530

    feat(day04): add Python functions and parameters examples

---
+++ day04/01_functions.py
@@ -0,0 +1,26 @@
+# Funtion
+
+# Without functions
+
+print("Hello")
+print("Hello")
+print("Hello")
+print("Hello")
+
+# Repeated code, Hard to maintain, More bugs
+
+# With Function (Exa)
+
+def greet():
+    print("Hello AI Enginner.")
+    
+# Use 
+greet()
+greet()
+
+# ----------------------
+
+def welcome():
+    print("Welcome to GenAI Engineering.")
+
+welcome()
---
+++ day04/02_parameters.py
@@ -0,0 +1,21 @@
+# Parameters
+
+# Exa 
+def welcome(role):
+    print(f"Hello {role}")
+    
+welcome("GenAI Enginner")
+
+def greet(name):
+    print(f"Hello {name}")
+    
+greet("Shubham")
+greet("Cyber")
+
+# Multiline Parameter
+
+def student(name, age):
+    print(name)
+    print(age)
+    
+student("Shubham", 24)
