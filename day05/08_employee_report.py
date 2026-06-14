12f9818ff8aec324baa5258f88123c28f8ecdd64
SRCarlo <asphaltshubhuu@gmail.com>
2026-06-14 10:06:09 +0530

    feat(day05): build employee report generator project

---
+++ day05/08_employee_report.py
@@ -0,0 +1,19 @@
+# Employee Report Generator
+
+employees = [
+    {
+        "name": "Shubham",
+        "role": "AI Enginner"
+    },
+    {
+        "name": "Aniket",
+        "role": "Backend Developer"
+    }
+]
+
+for employee in employees:
+    print(
+        employee["name"],
+        "-",
+        employee["role"]
+    )
