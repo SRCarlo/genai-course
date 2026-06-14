61f1adbb2eea3f6bbd70f01fa33b9ca2e9cf7a2f
SRCarlo <asphaltshubhuu@gmail.com>
2026-06-14 08:03:16 +0530

    feat(day05): implement range and enumerate examples

---
+++ day05/03_range.py
@@ -0,0 +1,16 @@
+# RANGE()
+
+for number in range(5):
+    print(number)
+    
+print("--------------------------")
+
+# Start and End
+for num in range(1,6):
+    print(num)
+    
+print("-----------------")
+
+# Step 
+for numbers in range(0,10,2):
+    print(numbers)
---
+++ day05/04_enumerate.py
@@ -0,0 +1,17 @@
+# ENUMERATE
+
+# WHY IMPORTANT?
+    # Used when:
+                # Need position
+                # Need numbering
+                # Need ranking
+                # Need indexing
+
+skills = [
+    "Python",
+    "Java",
+    "AI"
+]
+
+for index, skill in enumerate(skills):
+    print(index, skill)
