662096221b929dc0c2258abc0907e042d05bd4df
SRCarlo <asphaltshubhuu@gmail.com>
2026-06-15 08:48:10 +0530

    feat(day06): add conditional statement examples

---
+++ day06/02_if_else.py
@@ -0,0 +1,8 @@
+# IF ELSE
+
+age = 16 
+
+if age >= 18:
+    print("Eligible")
+else:
+    print("Not Eligible")
---
+++ day06/03_if_elif_else.py
@@ -0,0 +1,17 @@
+# IF ELIF ELSE
+        # Used for multiple conditions.
+
+marks = 95
+
+if marks >= 90:
+    print("A Grade")
+    
+elif marks >= 75:
+    print("B Grade")
+    
+elif marks >= 60:
+    print("C Grade")
+    
+else:
+    print("Fail")
+    
