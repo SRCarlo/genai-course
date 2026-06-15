1ccae0065094e50e473b8f861dee34e05755e27c
SRCarlo <asphaltshubhuu@gmail.com>
2026-06-15 09:22:49 +0530

    feat(day06): add user input validation examples

---
+++ day06/06_input_validation.py
@@ -0,0 +1,16 @@
+# INPUT VALIDATION
+
+age = int(input("Enter Age: "))
+
+if age >= 18:
+    print("Access Granted")
+else:
+    print("Access Denied")
+    
+
+# WHY IMPORTANT?        
+    # Without validation:
+                    # Wrong Data
+                    # Bad Input
+                    # System Errors
+                    # Security Risks
