34fa494dab7101b2239c9f9879e7e5989646d3d3
SRCarlo <asphaltshubhuu@gmail.com>
2026-06-17 22:12:24 +0530

    feat(day08): implement vector operations examples

---
+++ month01_foundations/week02_math_foundations/day08_linear_algebra/03_vector_operations.py
@@ -0,0 +1,27 @@
+# Vector Operations
+
+# Vector Addition
+
+v1 = [1, 2]
+
+v2 = [3, 4]
+
+result = [
+    v1[0] + v2[0],
+    v1[1] + v2[1]
+]
+
+print(result)
+
+# Vector Substraction
+
+v1 = [5, 4]
+
+v2 = [2, 1]
+
+result = [
+    v1[0] - v2[0],
+    v1[1] - v2[1]
+] 
+
+print(result)
