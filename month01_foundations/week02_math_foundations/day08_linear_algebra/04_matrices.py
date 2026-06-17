706f4d28e3dccf503604c11b6dcbe635ac96f8f5
SRCarlo <asphaltshubhuu@gmail.com>
2026-06-17 22:18:34 +0530

    feat(day08): add matrix fundamentals and operations

---
+++ month01_foundations/week02_math_foundations/day08_linear_algebra/04_matrices.py
@@ -0,0 +1,19 @@
+# Matrices 
+
+# What ios Matrix : A table of Numbers.
+
+matrix = [
+    [1, 2],
+    [3, 4]
+]
+
+print(matrix)
+
+
+# Why Important:
+        # Neural Networks
+        # Transformers
+        # Embeddings
+        # Image Processing
+        
+# all use matrices.
---
+++ month01_foundations/week02_math_foundations/day08_linear_algebra/05_matrix_operations.py
@@ -0,0 +1,13 @@
+# Matrix Operations
+
+# Access matrix values.
+matrix = [
+    [1, 2],
+    [3, 4]
+]
+
+print(matrix[0][0])
+
+# Print matrix
+for row in matrix:
+    print(row)
