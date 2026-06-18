ec2d723937eade4880655dc6587bdee67f0d6048
SRCarlo <asphaltshubhuu@gmail.com>
2026-06-18 09:03:03 +0530

    feat(day09): add probability fundamentals

---
+++ month01_foundations/week02_math_foundations/day09_probability/01_probability_basics.py
@@ -0,0 +1,14 @@
+# Probability
+
+# Probalbility : measure chance.
+
+# Example: Coin Toss
+
+# Possible outcomes:
+                    # Head
+                    # Tail
+
+# Probability of Head:  1 / 2 = 0.5
+
+head_probability = 1/2
+print(head_probability) # 0.5
