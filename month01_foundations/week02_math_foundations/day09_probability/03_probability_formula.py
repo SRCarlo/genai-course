e0b6990a159dc582dc25168321ebeb84eb638ff2
SRCarlo <asphaltshubhuu@gmail.com>
2026-06-18 10:13:52 +0530

    feat(day09): add conditional probability examples

---
+++ month01_foundations/week02_math_foundations/day09_probability/03_probability_formula.py
@@ -0,0 +1,13 @@
+
+
+# Example:
+# Probability of getting 2 from dice.
+
+# Favorable outcomes: 1
+
+# Total outcomes: 6
+
+# Calculation:
+probability = 1 / 6
+
+print(probability) # 0.1666666667
---
+++ month01_foundations/week02_math_foundations/day09_probability/04_conditional_probability.py
@@ -0,0 +1,6 @@
+# Conditional Probability
+    # Probability of event A given event B
+
+rain_given_clouds = 0.7
+
+print("Probability:", rain_given_clouds)
