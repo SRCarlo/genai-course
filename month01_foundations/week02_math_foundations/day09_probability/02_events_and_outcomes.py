b6efeb24f4d127d9053c5b76cc8d3ad44eb133bc
SRCarlo <asphaltshubhuu@gmail.com>
2026-06-18 09:52:17 +0530

    feat(day09): implement events and outcomes examples

---
+++ month01_foundations/week02_math_foundations/day09_probability/02_events_and_outcomes.py
@@ -0,0 +1,38 @@
+# Event and OutComes
+
+# Outcome
+
+"""
+# Single possible result.
+
+# Example:
+
+# Dice Roll
+
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+
+# Each number is an outcome.
+
+# Event
+    
+    Collection of outcomes.
+    
+    Example: Getting an Even Number
+                2
+                4
+                6
+
+This is an event.
+"""
+
+outcomes = [1, 2, 3, 4, 5, 6]
+
+even_numbers = [2, 4, 6]
+
+print(outcomes)
+print(even_numbers)
