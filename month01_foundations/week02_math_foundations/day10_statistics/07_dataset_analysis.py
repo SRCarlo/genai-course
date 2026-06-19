5ba7e6497a937ebfb91226b41d3b39e1345f982e
SRCarlo <asphaltshubhuu@gmail.com>
2026-06-19 14:18:41 +0530

    feat(day10): build dataset analysis program

---
+++ month01_foundations/week02_math_foundations/day10_statistics/07_dataset_analysis.py
@@ -0,0 +1,9 @@
+# Mini Data Analysis
+
+from statistics import (mean, median, mode)
+
+data = [10, 20, 20, 30, 40]
+
+print("Mean:", mean(data))
+print("Median:", median(data))
+print("Mode:", mode(data))
