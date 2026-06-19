51e33db47deaa065b9e533f1695f21706c399940
SRCarlo <asphaltshubhuu@gmail.com>
2026-06-19 14:01:52 +0530

    feat(day10): add mean and median calculations

---
+++ month01_foundations/week02_math_foundations/day10_statistics/01_mean.py
@@ -0,0 +1,20 @@
+# Mean (Average)
+
+'''
+Formula:
+    Sum of Values
+    ---------------
+    Total Values
+    
+Example:10, 20, 30, 40, 50
+
+Calculation:    (10+20+30+40+50)/5
+                150/5
+                30
+'''
+
+numbers = [10, 20, 30 , 40, 50]
+
+mean = sum(numbers) / len(numbers)
+
+print("Mean:", mean)
---
+++ month01_foundations/week02_math_foundations/day10_statistics/02_median.py
@@ -0,0 +1,11 @@
+# Median
+
+# Middle value after sorting.
+
+numbers = [5, 10, 15, 20, 25]
+
+middle = len(numbers) // 2
+
+median = numbers[middle]
+
+print("Median:", median)
