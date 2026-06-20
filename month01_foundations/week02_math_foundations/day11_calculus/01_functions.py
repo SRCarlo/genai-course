3f28e939b7822ffd7c95626b4628ddd6b3f96d67
SRCarlo <asphaltshubhuu@gmail.com>
2026-06-20 08:51:04 +0530

    feat(day11): add function and slope fundamentals

---
+++ month01_foundations/week02_math_foundations/day11_calculus/01_functions.py
@@ -0,0 +1,22 @@
+# Function
+
+#Function: A relationship between input and output.
+
+def square(x):
+    return x * x
+
+print(square(5))
+
+'''
+
+# Input → Function → Output
+
+# AI Example
+
+User Prompt
+      ↓
+Neural Network
+      ↓
+Prediction
+
+'''
---
+++ month01_foundations/week02_math_foundations/day11_calculus/slope.py
@@ -0,0 +1,40 @@
+'''
+# Slope
+
+# Slope : Measure steepness
+
+# Formula :
+
+Slope =
+        Change in Y
+        -----------
+        Change in X
+        
+Example:
+Point A = (1,2)
+Point B = (3,6)
+
+
+Calculation:
+            (6 - 2)
+            ---------
+            (3 - 1)
+
+            4 / 2
+
+            2
+'''
+
+x1 = 1
+y1 = 2
+
+x2 = 3
+y2 = 6
+
+slope = (y2 - y1) / (x2 - x1)
+
+print(slope)
+
+
+
+
