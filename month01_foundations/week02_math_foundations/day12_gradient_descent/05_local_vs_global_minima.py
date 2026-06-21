5809c07c8f5b3b118b5796f71344f277a00fdb99
SRCarlo <asphaltshubhuu@gmail.com>
2026-06-21 08:15:39 +0530

    feat(day12): explain local and global minima concepts

---
+++ month01_foundations/week02_math_foundations/day12_gradient_descent/05_local_vs_global_minima.py
@@ -0,0 +1,26 @@
+"""
+Local Minimum : Small valley.
+
+Global Minimum : Lowest valley.
+
+
+Visual:
+
+      /\
+
+     /  \
+
+    /    \___
+   /         \__
+__/             \____
+
+Local      Global
+
+
+
+AI Goal: Reach Global Minimum
+
+Because: Lowest Error
+
+"""
+
