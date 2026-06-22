efde48e7f74fa989de84ad1a8bca9d38ad8a506b
SRCarlo <asphaltshubhuu@gmail.com>
2026-06-22 11:43:07 +0530

    feat(day13): add prediction evaluation examples

---
+++ month01_foundations/week02_math_foundations/day13_ml_mini_project/07_evaluation.py
@@ -0,0 +1,19 @@
+"""
+Evaluation means:
+
+How close prediction is to actual value
+"""
+
+actual = 75
+
+predicted = 80
+
+error = abs(
+    actual -
+    predicted
+)
+
+print(
+    "Error:",
+    error
+)
