b6baeef760c9ed73f28c7577cfe7aacbf8316733
SRCarlo <asphaltshubhuu@gmail.com>
2026-06-22 11:35:27 +0530

    feat(day13): add statistical analysis

---
+++ month01_foundations/week02_math_foundations/day13_ml_mini_project/04_statistics.py
@@ -0,0 +1,21 @@
+from statistics import mean
+
+hours = [
+    1,2,3,4,5,
+    6,7,8,9,10
+]
+
+scores = [
+    10,20,30,40,50,
+    60,70,80,90,100
+]
+
+print(
+    "Average Hours:",
+    mean(hours)
+)
+
+print(
+    "Average Score:",
+    mean(scores)
+)
