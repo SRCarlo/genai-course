50add44f03448f317668623aa904fd289f239e01
SRCarlo <asphaltshubhuu@gmail.com>
2026-06-22 11:23:26 +0530

    feat(day13): add student dataset

---
+++ month01_foundations/week02_math_foundations/day13_ml_mini_project/02_data_loading.py
@@ -0,0 +1,13 @@
+import csv 
+
+with open(
+    "data/student_data.csv",
+          "r"
+    ) as file:
+    
+    reader = csv.reader(file)
+    
+    for row in reader:
+        print(row)
+    
+    
