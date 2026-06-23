94b70c4f2216811745f6ac5095fa307de1699830
SRCarlo <asphaltshubhuu@gmail.com>
2026-06-23 09:40:58 +0530

    Add Week 2 AI math final revision project

---
+++ month01_foundations/week02_math_foundations/day14_revision/08_week2_final_project.py
@@ -0,0 +1,15 @@
+print("____________AI MATH REVISION PROJECT_____________")
+
+hours = float(
+    input("Study Hours: ")
+)
+
+score = hours * 10
+
+probability = 0.8
+
+print(f"""
+Predicted Score : {score}
+
+Success Probability : {probability}
+""")
