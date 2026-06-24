91c8290a25323c515fb50805dd6cc6771beb9984
SRCarlo <asphaltshubhuu@gmail.com>
2026-06-24 13:42:07 +0530

    Create model evaluation example using prediction error

---
+++ month01_foundations/week03_machine_learning/07_model_evaluation.py
@@ -0,0 +1,16 @@
+'''
+Why Evaluate?
+    Need to know:
+            Is model good?
+            
+Formula: Error = Actual - Prediction
+
+'''
+
+actual = 80
+
+predicted = 75
+
+error = abs(actual - predicted)
+
+print(error)
