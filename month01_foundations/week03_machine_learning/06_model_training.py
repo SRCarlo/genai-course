f300f15ffc3f933560c5e9f11cba40436a4e161a
SRCarlo <asphaltshubhuu@gmail.com>
2026-06-24 13:41:57 +0530

    Add basic model training workflow example

---
+++ month01_foundations/week03_machine_learning/06_model_training.py
@@ -0,0 +1,20 @@
+'''
+What is Training?
+    Learning patterns from data.
+
+Example:
+        2 Hours → 20
+        5 Hours → 50
+        8 Hours → 80
+
+Pattern:
+        Score = Hours × 10
+        
+'''
+def train():
+
+    print(
+        "Model Trained"
+    )
+
+train()
