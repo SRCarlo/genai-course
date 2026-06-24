05e1e59a2f7c9a09849b51c9c2795df46dd85947
SRCarlo <asphaltshubhuu@gmail.com>
2026-06-24 13:41:44 +0530

    Create feature engineering demonstration using student features

---
+++ month01_foundations/week03_machine_learning/04_feature_engineering.py
@@ -0,0 +1,28 @@
+'''
+What is Feature?
+    Input Variable.
+
+Example:
+        Study Hours
+        Attendance
+        Assignments
+
+These are Features.
+
+Why Important?
+
+Bad Features: Student Name
+
+Good Features:
+        Hours Studied
+        Attendance
+
+'''
+
+
+student = {
+    "hours": 8,
+    "attendence": 90
+}
+
+print(student)
