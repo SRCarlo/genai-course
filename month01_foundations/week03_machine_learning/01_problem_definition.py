18eae9de86d63549ca36ccc4a59f215306ace386
SRCarlo <asphaltshubhuu@gmail.com>
2026-06-24 13:39:41 +0530

    Add problem definition example for student score prediction

---
+++ month01_foundations/week03_machine_learning/01_problem_definition.py
@@ -0,0 +1,22 @@
+'''
+What is Problem Definition?
+
+Before coding:
+Ask: What problem are we solving?
+
+Example: Predict Student Scores
+
+Input: Study Hours
+
+Output: Expected Score
+
+'''
+
+
+
+problem = """
+Predict student score
+based on study hours.
+"""
+
+print(problem)
