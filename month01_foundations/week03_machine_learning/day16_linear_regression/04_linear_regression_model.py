045287b556e250e9af5f8f0a9149f761c05733a8
SRCarlo <asphaltshubhuu@gmail.com>
2026-06-25 10:46:27 +0530

    feat(day16): train linear regression model using scikit-learn

---
+++ month01_foundations/week03_machine_learning/day16_linear_regression/04_linear_regression_model.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+from sklearn.linear_model import (
+    LinearRegression
+)
+
+df = pd.read_csv(
+    "data/student_scores.csv"
+)
+
+X = df[["hours"]]
+
+y = df["score"]
+
+model = LinearRegression()
+
+model.fit(X, y)
+
+print("Training Complete")
