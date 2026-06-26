6ec3d50a0415219791e3d6167aadaf1e6b60cb6e
SRCarlo <asphaltshubhuu@gmail.com>
2026-06-26 12:43:41 +0530

    feat(day17): implement prediction using logistic regression

---
+++ month01_foundations/week03_machine_learning/day17_classification/06_prediction.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+from sklearn.linear_model import (
+    LogisticRegression
+)
+
+df = pd.read_csv("data/student_pass_fail.csv")
+
+df["result"] = ( df["result"].map({
+        "Fail":0,
+        "Pass":1
+    })
+)
+
+X = df[["hours"]]
+
+y = df["result"]
+
+model = LogisticRegression()
+
+model.fit(X,y)
+
+prediction = model.predict( [[7]])
+
+print(prediction)
