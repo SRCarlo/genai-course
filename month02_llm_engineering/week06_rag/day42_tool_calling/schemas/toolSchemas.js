68cf8ea117ee77517fa0c010df8f938abaae545d
SRCarlo <asphaltshubhuu@gmail.com>
2026-07-24 21:45:59 +0530

    feat(day42): create tool registry

---
+++ month02_llm_engineering/week06_rag/day42_tool_calling/schemas/toolSchemas.js
@@ -0,0 +1,31 @@
+export const tools = [
+  {
+    name: "calculator",
+
+    description: "Perform mathematical calculations",
+
+    parameters: {
+      operation: "string",
+
+      a: "number",
+
+      b: "number",
+    },
+  },
+
+  {
+    name: "weather",
+
+    description: "Get weather information",
+
+    parameters: {
+      city: "string",
+    },
+  },
+
+  {
+    name: "github",
+
+    description: "Get GitHub repository information",
+  },
+];
