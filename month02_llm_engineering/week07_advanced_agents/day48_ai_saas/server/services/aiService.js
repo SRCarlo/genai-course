748cf1683eed78f0caf8b408c95bf0c30e5b7569
SRCarlo <asphaltshubhuu@gmail.com>
2026-07-31 12:17:44 +0530

    feat(day48): integrate AI service layer

---
+++ month02_llm_engineering/week07_advanced_agents/day48_ai_saas/server/services/aiService.js
@@ -0,0 +1,20 @@
+import Groq from "groq-sdk";
+
+export async function askAI(prompt) {
+  const groq = new Groq({
+    apiKey: process.env.GROQ_API_KEY,
+  });
+
+  const response = await groq.chat.completions.create({
+    messages: [
+      {
+        role: "user",
+        content: prompt,
+      },
+    ],
+
+    model: "llama-3.3-70b-versatile",
+  });
+
+  return response.choices[0].message.content;
+}
